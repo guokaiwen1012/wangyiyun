53b60fffe0128b797e77178e4f119d53877798c2
”guokaiwen1012 <942314190@qq.com>
2019-01-03 09:01:24 +0800

    shanchuanImg

---
+++ updateLicence/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- <button @click="login">点击登陆</button> -->
+  <router-view></router-view>
+</template>
+
+<script>
+import JSBridge from './utils/JSBridge.js';
+export default {
+  name: 'app',
+  data () {
+    return {}
+  },
+  methods: {
+    login(){
+      JSBridge.invoke('app', 'login', {
+        loginCallBackName: (res)=>{
+          console.log('res...', res);
+        }
+      });
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './scss/common.scss';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
